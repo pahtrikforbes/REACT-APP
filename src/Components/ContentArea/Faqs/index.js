e7426a2a796d8960ae7591ac5e540878077db2fe
abdulmughni <abdul.mughni94@gmail.com>
2019-08-20 00:39:22 +0500

    Changes

--- src/Components/ContentArea/Faqs/index.js
+++ src/Components/ContentArea/Faqs/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBBtn, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBRow, MDBCol  } from 'mdbreact';
+import { MDBContainer, MDBBtn, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBRow, MDBCol } from 'mdbreact';
 import AllPopupContact from '../AllPopupContact';
 import FaqSearchForm from './FaqSearchForm.js';
 import FaqSlider from './FaqSlider.js';
@@ -9,33 +9,33 @@ import Logo from '../../../statics/images/logo.png';
 
 class Faqs extends Component {
 
-render() {
-  const { closeModal } = this.props;
+  render() {
+    const { closeModal } = this.props;
 
-  return (
-    <MDBContainer>
-      <MDBModalHeader className="p-modal-header">
+    return (
+      <MDBContainer>
+        <MDBModalHeader className="p-modal-header">
           <div className="popup-header">
-            <MDBIcon icon="long-arrow-alt-left" onClick={ closeModal }/>
+            <MDBIcon icon="long-arrow-alt-left" onClick={closeModal} />
             <img src={Logo} alt="Rate Hogs" className="img-fluid" />
           </div>
-          Contact Us
+          FAQ
       </MDBModalHeader>
-      <MDBModalBody>
-        <MDBContainer>
-          <MDBRow>
-            <MDBCol md="3"><AllPopupContact /></MDBCol>
-            <MDBCol md="9">
+        <MDBModalBody>
+          <MDBContainer>
+            <MDBRow>
+              <MDBCol md="3"><AllPopupContact /></MDBCol>
+              <MDBCol md="9">
                 <div className="contact-us">
                   <FaqSearchForm />
                   <FaqSlider />
                   <FaqAccordion />
                 </div>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </MDBModalBody>
-    </MDBContainer>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </MDBModalBody>
+      </MDBContainer>
     );
   }
 }
